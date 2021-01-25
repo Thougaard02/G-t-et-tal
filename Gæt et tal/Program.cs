c3bd211e99647373652fae235170316ae2b56056
Rasmus <Rasmus@vinperlen.dk>
2021-01-25 13:14:21 +0100

    Add project files.

---
+++ Gæt et tal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gæt_et_tal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int securedNumber = r.Next(1, 11);
+            int guessNumber = 0;
+            while (true)
+            {
+                Console.WriteLine("Gæt et tal mellem 1-10");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess < securedNumber)
+                {
+                    Console.WriteLine("Højere");
+                    guessNumber++;
+                    Console.WriteLine($"Du har brugt {guessNumber} forsøg");
+                }
+                else if (guess > securedNumber)
+                {
+                    Console.WriteLine("Lavere");
+                    guessNumber++;
+                    Console.WriteLine($"Du har brugt {guessNumber} forsøg");
+                }
+                else if (guess == securedNumber)
+                {
+                    Console.WriteLine("Godt gættet!");
+                    guessNumber++;
+                    Console.WriteLine($"Du gættet tallet på {guessNumber} forsøg");
+                }
+            }
+        }
+    }
+}
